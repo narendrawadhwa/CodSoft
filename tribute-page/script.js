43312e1b6582f636a0e48da9576b2bb7d0e56557
narendrawadhwa <narencreation101002@gmail.com>
2023-09-20 06:05:51 +0530

    changes made

---
+++ tribute-page/script.js
@@ -0,0 +1,50 @@
+const slides = document.querySelectorAll(".slide");
+let counter = -1; // Start with -1 to indicate no image is selected initially
+
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`;
+});
+
+const goPrev = () => {
+    if (counter > 0) {
+        slides[counter].classList.remove("active");
+        counter--;
+        slideImage();
+    }
+};
+
+const goNext = () => {
+    if (counter < slides.length - 1) {
+        if (counter >= 0) {
+            slides[counter].classList.remove("active");
+        }
+        counter++;
+        slideImage();
+    }
+};
+
+const slideImage = () => {
+    slides.forEach((slide, index) => {
+        if (index === counter) {
+            slide.classList.add("active");
+        } else {
+            slide.classList.remove("active");
+        }
+    });
+};
+
+// Initially, call goNext to display the first image
+goNext();
+
+
+//
+let subMenu = document.getElementById("subMenu");
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+}
+
+
+//
+const audio = document.getElementById('background-audio');
+audio.play();
+
